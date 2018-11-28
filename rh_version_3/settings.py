d543af7fdd9b189e9fd5d909096c3561c4d0369b
ggermando <germainkataku@outlook.com>
2018-11-28 16:22:28 +0100

    modification

--- rh_version_3/settings.py
+++ rh_version_3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ip7ui-dh_bed-=bm#f07rs6s%i#t4spt7(4l&$ktvpo7@v(k@r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.13', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -47,6 +47,13 @@ INSTALLED_APPS = [
     'agent',
     'projet',
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
 
 PAGINATION_SETTINGS = {
     'PAGE_RANGE_DISPLAYED': 10,
@@ -89,10 +96,21 @@ WSGI_APPLICATION = 'rh_version_3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'kataku',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
